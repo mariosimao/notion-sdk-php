d6314aad1acc7e4fb730f25af31c5779ecd9e456
Mario Simão <mariosimao@poli.ufrj.br>
2021-10-21 14:57:30 -0300

    Add page parent unit tests

---
+++ tests/Unit/Pages/PageParentTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Notion\Test\Unit\Pages;
+
+use Notion\Pages\PageParent;
+use PHPUnit\Framework\TestCase;
+
+class PageParentTest extends TestCase
+{
+    public function test_create_parent_database(): void
+    {
+        $parent = PageParent::database("058d158b-09de-4d69-be07-901c20a7ca5c");
+
+        $this->assertTrue($parent->isDatabase());
+        $this->assertEquals("058d158b-09de-4d69-be07-901c20a7ca5c", $parent->id());
+    }
+
+    public function test_create_parent_page(): void
+    {
+        $parent = PageParent::page("058d158b-09de-4d69-be07-901c20a7ca5c");
+
+        $this->assertTrue($parent->isPage());
+        $this->assertEquals("058d158b-09de-4d69-be07-901c20a7ca5c", $parent->id());
+    }
+
+    public function test_create_parent_workspace(): void
+    {
+        $parent = PageParent::workspace();
+
+        $this->assertTrue($parent->isWorkspace());
+        $this->assertEquals("workspace", $parent->type());
+    }
+
+    public function test_page_array_conversion(): void
+    {
+        $array = [
+            "type" => "page_id",
+            "page_id" => "7a774b5d-ca74-4679-9f18-689b5a98f138",
+        ];
+        $parent = PageParent::fromArray($array);
+
+        $this->assertEquals($array, $parent->toArray());
+    }
+
+    public function test_database_array_conversion(): void
+    {
+        $array = [
+            "type" => "database_id",
+            "database_id" => "7a774b5d-ca74-4679-9f18-689b5a98f138",
+        ];
+        $parent = PageParent::fromArray($array);
+
+        $this->assertEquals($array, $parent->toArray());
+    }
+
+    public function test_workspace_array_conversion(): void
+    {
+        $array = [
+            "type" => "workspace",
+            "workspace" => true,
+        ];
+        $parent = PageParent::fromArray($array);
+
+        $this->assertEquals($array, $parent->toArray());
+    }
+
+    public function test_invalid_type_array(): void
+    {
+        $this->expectException(\Exception::class);
+        PageParent::fromArray([ "type" => "invalid-type" ]);
+    }
+}
