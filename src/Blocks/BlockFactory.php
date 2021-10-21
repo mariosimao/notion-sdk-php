50e012bd3c2d7b312d7091c1202754a06a592363
Mario Simão <mariosimao@poli.ufrj.br>
2021-10-21 16:15:46 -0300

    Add missing blocks to BlockFactory

--- src/Blocks/BlockFactory.php
+++ src/Blocks/BlockFactory.php
@@ -14,10 +14,17 @@ class BlockFactory
         $type = $array["type"];
 
         return match($type) {
-            Block::TYPE_PARAGRAPH => Paragraph::fromArray($array),
-            Block::TYPE_HEADING_1 => Heading1::fromArray($array),
-            Block::TYPE_HEADING_2 => Heading2::fromArray($array),
-            Block::TYPE_HEADING_3 => Heading3::fromArray($array),
+            Block::TYPE_PARAGRAPH          => Paragraph::fromArray($array),
+            Block::TYPE_HEADING_1          => Heading1::fromArray($array),
+            Block::TYPE_HEADING_2          => Heading2::fromArray($array),
+            Block::TYPE_HEADING_3          => Heading3::fromArray($array),
+            Block::TYPE_CALLOUT            => Callout::fromArray($array),
+            Block::TYPE_QUOTE              => Quote::fromArray($array),
+            Block::TYPE_BULLETED_LIST_ITEM => BulletedListItem::fromArray($array),
+            Block::TYPE_NUMBERED_LIST_ITEM => NumberedListItem::fromArray($array),
+            Block::TYPE_TO_DO              => ToDo::fromArray($array),
+            Block::TYPE_TOGGLE             => Toggle::fromArray($array),
+            Block::TYPE_CODE               => Code::fromArray($array),
             default => throw new Exception("Invalid block type '{$type}'"),
         };
     }
