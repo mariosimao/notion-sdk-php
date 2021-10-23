4c56fbda3c6b377c808284440cd1a4bc09afafa2
Mario Simão <mariosimao@poli.ufrj.br>
2021-10-23 18:48:48 -0300

    Add users integration tests

---
+++ tests/Integration/UsersTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Notion\Test\Integration;
+
+use Notion\Client;
+use Notion\NotionException;
+use PHPUnit\Framework\TestCase;
+
+class UsersTest extends TestCase
+{
+    public function test_find_current_user(): void
+    {
+        $token = getenv("NOTION_TOKEN");
+        $client = Client::create($token);
+
+        $user = $client->users()->me();
+        $sameUser = $client->users()->find($user->id());
+
+        $this->assertTrue($user->isBot());
+        $this->assertEquals($user, $sameUser);
+    }
+
+    public function test_find_all_users(): void
+    {
+        $token = getenv("NOTION_TOKEN");
+        $client = Client::create($token);
+
+        $users = $client->users()->findAll();
+
+        $this->assertCount(2, $users);
+        $this->assertTrue($users[0]->isPerson());
+        $this->assertTrue($users[1]->isBot());
+    }
+
+    public function test_find_inexistent_user(): void
+    {
+        $token = getenv("NOTION_TOKEN");
+        $client = Client::create($token);
+
+        $this->expectException(NotionException::class);
+        $this->expectExceptionMessage(
+            "Could not find user with ID: 7c3bd31e-63fa-4c60-956d-2264ceb2c522."
+        );
+        $client->users()->find("7c3bd31e-63fa-4c60-956d-2264ceb2c522");
+    }
+}
