8227e26e901838a85c8edb458e223924aabf68bb
Mario Simão <mariosimao@poli.ufrj.br>
2021-10-30 21:37:41 -0300

    Add date page property

---
+++ src/Pages/Properties/Date.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Notion\Pages\Properties;
+
+use DateTimeImmutable;
+use Notion\Common\Date as CommonDate;
+use Notion\Common\RichText;
+
+/**
+ * @psalm-type DateJson = array{
+ *      id: string,
+ *      type: "date",
+ *      date: array{
+ *          start: string,
+ *          end?: string,
+ *      },
+ * }
+ */
+class Date implements PropertyInterface
+{
+    private const TYPE = Property::TYPE_DATE;
+
+    private Property $property;
+
+    private DateTimeImmutable $start;
+    private DateTimeImmutable|null $end;
+
+    private function __construct(
+        Property $property,
+        DateTimeImmutable $start,
+        DateTimeImmutable|null $end,
+    ) {
+        $this->property = $property;
+        $this->start = $start;
+        $this->end = $end;
+    }
+
+    public static function create(
+        DateTimeImmutable $start,
+        DateTimeImmutable|null $end = null,
+    ): self {
+        $property = Property::create("", self::TYPE);
+
+        return new self($property, $start, $end);
+    }
+
+    public static function fromArray(array $array): self
+    {
+        /** @psalm-var DateJson $array */
+
+        $property = Property::fromArray($array);
+
+        $start = new DateTimeImmutable($array[self::TYPE]["start"]);
+        $end = isset($array[self::TYPE]["end"]) ?
+            new DateTimeImmutable($array[self::TYPE]["end"]) : null;
+
+        return new self($property, $start, $end);
+    }
+
+    public function toArray(): array
+    {
+        $array = $this->property->toArray();
+
+        $array[self::TYPE] = [
+            "start" => $this->start->format(CommonDate::FORMAT),
+            "end"   => $this->end?->format(CommonDate::FORMAT),
+        ];
+
+        return $array;
+    }
+
+    public function property(): Property
+    {
+        return $this->property;
+    }
+
+    public function start(): DateTimeImmutable
+    {
+        return $this->start;
+    }
+
+    public function end(): DateTimeImmutable|null
+    {
+        return $this->end;
+    }
+
+    public function withStart(DateTimeImmutable $start): self
+    {
+        return new self($this->property, $start, $this->end);
+    }
+
+    public function withEnd(DateTimeImmutable $end): self
+    {
+        return new self($this->property, $this->start, $end);
+    }
+
+    public function withoutEnd(): self
+    {
+        return new self($this->property, $this->start, null);
+    }
+
+    public function isRange(): bool
+    {
+        return $this->end !== null;
+    }
+}
---
+++ tests/Unit/Pages/Properties/DateTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Notion\Test\Unit\Pages\Properties;
+
+use DateTimeImmutable;
+use Notion\Pages\Properties\Date;
+use PHPUnit\Framework\TestCase;
+
+class DateTest extends TestCase
+{
+    public function test_create_date(): void
+    {
+        $someday = new DateTimeImmutable("2021-01-01");
+
+        $date = Date::create($someday);
+
+        $this->assertEquals($someday, $date->start());
+        $this->assertNull($date->end());
+        $this->assertFalse($date->isRange());
+        $this->assertEquals("date", $date->property()->type());
+    }
+
+    public function test_create_range(): void
+    {
+        $start = new DateTimeImmutable("2021-01-01");
+        $end = new DateTimeImmutable("2021-12-31");
+
+        $date = Date::create($start, $end);
+
+        $this->assertTrue($date->isRange());
+        $this->assertEquals($start, $date->start());
+        $this->assertEquals($end, $date->end());
+    }
+
+    public function test_change_start(): void
+    {
+        $newStart = new DateTimeImmutable("2021-01-01");
+
+        $date = Date::create(new DateTimeImmutable("2020-01-01"))
+                    ->withStart($newStart);
+
+        $this->assertEquals($newStart, $date->start());
+    }
+
+    public function test_change_end(): void
+    {
+        $newEnd = new DateTimeImmutable("2021-12-31");
+
+        $date = Date::create(new DateTimeImmutable("2021-01-01"))
+                    ->withEnd($newEnd);
+
+        $this->assertEquals($newEnd, $date->end());
+    }
+
+    public function test_remove_end(): void
+    {
+        $date = Date::create(
+            new DateTimeImmutable("2021-01-01"),
+            new DateTimeImmutable("2021-12-31"),
+        )->withoutEnd();
+
+        $this->assertNull($date->end());
+        $this->assertFalse($date->isRange());
+    }
+
+    public function test_array_conversion(): void
+    {
+        $array = [
+            "id"   => "a7ede3b7-c7ae-4eb8-b415-a7f80ac4dfe5",
+            "type" => "date",
+            "date" => [
+                "start" => "2021-01-01T00:00:00.000000Z",
+                "end"   => "2021-12-31T00:00:00.000000Z",
+            ],
+        ];
+        $date = Date::fromArray($array);
+
+        $this->assertEquals($array, $date->toArray());
+    }
+}
