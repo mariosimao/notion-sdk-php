69c63bb557faa3fd63088739bc767349435af5e9
Mario Simão <mariosimao@poli.ufrj.br>
2021-10-31 19:08:46 -0300

    Add database parent unit tests

---
+++ tests/Unit/Databases/DatabaseParentTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Notion\Test\Unit\Databases;
+
+use Notion\Databases\DatabaseParent;
+use PHPUnit\Framework\TestCase;
+
+class DatabaseParentTest extends TestCase
+{
+    public function test_create_parent_page(): void
+    {
+        $parent = DatabaseParent::page("058d158b-09de-4d69-be07-901c20a7ca5c");
+
+        $this->assertTrue($parent->isPage());
+        $this->assertEquals("058d158b-09de-4d69-be07-901c20a7ca5c", $parent->id());
+    }
+
+    public function test_create_parent_workspace(): void
+    {
+        $parent = DatabaseParent::workspace();
+
+        $this->assertTrue($parent->isWorkspace());
+        $this->assertEquals("workspace", $parent->type());
+    }
+
+    public function test_page_array_conversion(): void
+    {
+        $array = [
+            "type" => "page_id",
+            "page_id" => "7a774b5d-ca74-4679-9f18-689b5a98f138",
+        ];
+        $parent = DatabaseParent::fromArray($array);
+
+        $this->assertEquals($array["page_id"], $parent->toArray()["page_id"]);
+    }
+
+    public function test_workspace_array_conversion(): void
+    {
+        $array = [
+            "type" => "workspace",
+            "workspace" => true,
+        ];
+        $parent = DatabaseParent::fromArray($array);
+
+        $this->assertEquals($array["workspace"], $parent->toArray()["workspace"]);
+    }
+
+    public function test_invalid_type_array(): void
+    {
+        $this->expectException(\Exception::class);
+        DatabaseParent::fromArray([ "type" => "invalid-type" ]);
+    }
+}
