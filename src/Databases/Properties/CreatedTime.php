5fde74f1b220fe389dcf021eaee86d7d6449be29
Mario Simão <mariosimao@poli.ufrj.br>
2021-11-01 00:13:28 -0300

    Add created time database property

---
+++ src/Databases/Properties/CreatedTime.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Notion\Databases\Properties;
+
+/**
+ * @psalm-type CreatedTimeJson = array{
+ *      id: string,
+ *      name: string,
+ *      type: "created_time",
+ *      created_time: array<empty, empty>,
+ * }
+ */
+class CreatedTime implements PropertyInterface
+{
+    private const TYPE = Property::TYPE_CREATED_TIME;
+
+    private Property $property;
+
+    private function __construct(Property $property)
+    {
+        $this->property = $property;
+    }
+
+    public static function create(string $propertyName = "CreatedTime"): self
+    {
+        $property = Property::create("", $propertyName, self::TYPE);
+
+        return new self($property);
+    }
+
+    public function property(): Property
+    {
+        return $this->property;
+    }
+
+    public static function fromArray(array $array): self
+    {
+        /** @psalm-var CreatedTimeJson $array */
+        $property = Property::fromArray($array);
+
+        return new self($property);
+    }
+
+    public function toArray(): array
+    {
+        $array = $this->property->toArray();
+        $array[self::TYPE] = [];
+
+        return $array;
+    }
+}
---
+++ tests/Unit/Databases/Properties/CreatedTimeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Notion\Test\Unit\Databases\Properties;
+
+use Notion\Databases\Properties\Factory;
+use Notion\Databases\Properties\CreatedTime;
+use PHPUnit\Framework\TestCase;
+
+class CreatedTimeTest extends TestCase
+{
+    public function test_create(): void
+    {
+        $createdTime = CreatedTime::create("Dummy prop name");
+
+        $this->assertEquals("Dummy prop name", $createdTime->property()->name());
+        $this->assertTrue($createdTime->property()->isCreatedTime());
+    }
+
+    public function test_array_conversion(): void
+    {
+        $array = [
+            "id"    => "abc",
+            "name"  => "dummy",
+            "type"  => "created_time",
+            "created_time" => [],
+        ];
+        $createdTime = CreatedTime::fromArray($array);
+        $fromFactory = Factory::fromArray($array);
+
+        $this->assertEquals($array, $createdTime->toArray());
+        $this->assertEquals($array, $fromFactory->toArray());
+    }
+}
