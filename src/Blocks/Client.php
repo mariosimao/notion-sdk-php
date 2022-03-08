a6851488482ec41247205f69df3121f0b77308eb
Mario Simão <mariosimao@poli.ufrj.br>
2022-03-07 23:43:06 -0300

    Add find block

--- src/Blocks/Client.php
+++ src/Blocks/Client.php
@@ -29,6 +29,30 @@ class Client
         $this->version = $version;
     }
 
+    public function find(string $blockId): BlockInterface
+    {
+        $url = "https://api.notion.com/v1/blocks/{$blockId}";
+        $request = $this->requestFactory->createRequest("GET", $url)
+            ->withHeader("Authorization", "Bearer {$this->token}")
+            ->withHeader("Notion-Version", $this->version);
+
+        $response = $this->psrClient->sendRequest($request);
+
+        /** @var array */
+        $body = json_decode((string) $response->getBody(), true);
+
+        if ($response->getStatusCode() !== 200) {
+            /** @var array{ message: string, code: string} $body */
+            $message = $body["message"];
+            $code = $body["code"];
+
+            throw new NotionException($message, $code);
+        }
+
+        /** @var array{ type: string } $body */
+        return BlockFactory::fromArray($body);
+    }
+
     /** @return list<BlockInterface> */
     public function findChildren(string $blockId): array
     {
