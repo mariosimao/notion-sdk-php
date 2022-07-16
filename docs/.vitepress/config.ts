a1d41d5e4b65bb1684ac70ab68d4ba97656e3e4c
Mario Simão <mariosimao@poli.ufrj.br>
2022-07-16 20:52:04 -0300

    Add favicons (#95)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -38,6 +38,14 @@ export default defineConfig({
     description: 'A complete Notion SDK for PHP developers.',
     lang: 'en-US',
     lastUpdated: true,
+    head: [
+        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/favicons/apple-touch-icon.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png"}],
+        ['link', { rel: "manifest", href: "/favicons/site.webmanifest"}],
+        ['link', { rel: "shortcut icon", href: "/favicons/favicon.ico"}],
+        ['meta', { name: "theme-color", content: "#787CB5"}],
+    ],
     themeConfig: {
         logo: '/logo.png',
         nav: [
