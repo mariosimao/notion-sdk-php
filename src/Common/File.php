30e37b39074a1210d76810c677169899d688eb60
Matthew <matthew.boehm@percona.com>
2025-10-17 20:47:48 +0000

    add support for file_upload type

--- src/Common/File.php
+++ src/Common/File.php
@@ -8,9 +8,10 @@ use DateTimeImmutable;
  * @psalm-import-type RichTextJson from \Notion\Common\RichText
  *
  * @psalm-type FileJson = array{
- *      type: "external"|"file",
+ *      type: "external"|"file"|"file_upload",
  *      file?: array{ url: string, expiry_time: string },
  *      external?: array{ url: string },
+ *      file_upload?: array{ id: string },
  *      name?: string,
  *      caption?: list<RichTextJson>
  * }
@@ -22,7 +23,8 @@ class File
     /** @param RichText[] $caption */
     private function __construct(
         public readonly FileType $type,
-        public readonly string $url,
+        public readonly string|null $url,
+        public readonly string|null $fileId,
         public readonly DateTimeImmutable|null $expiryTime,
         public readonly string|null $name,
         public readonly array $caption,
@@ -31,14 +33,19 @@ class File
 
     public static function createExternal(string $url): self
     {
-        return new self(FileType::External, $url, null, null, []);
+        return new self(FileType::External, $url, null, null, null, []);
     }
 
     public static function createInternal(
         string $url,
         DateTimeImmutable|null $expiryTime = null
     ): self {
-        return new self(FileType::Internal, $url, $expiryTime, null, []);
+        return new self(FileType::Internal, $url, null, $expiryTime, null, []);
+    }
+
+    public static function createFileUpload(string $fileId): self
+    {
+        return new self(FileType::FileUpload, null, $fileId, null, null, []);
     }
 
     /**
@@ -55,6 +62,7 @@ class File
         return new self(
             FileType::from($type),
             $file["url"] ?? "",
+            $file["id"] ?? "",
             isset($file["expiry_time"]) ? new DateTimeImmutable($file["expiry_time"]) : null,
             $array["name"] ?? null,
             isset($array["caption"]) ? array_map(fn($c) => RichText::fromArray($c), $array["caption"]) : [],
@@ -73,6 +81,13 @@ class File
                     "url" => $this->url,
                     "expiry_time" => $this->expiryTime?->format(Date::FORMAT),
                 ],
+            ];
+        }
+
+        if ($type === FileType::FileUpload) {
+            $array = [
+                "type" => "file_upload",
+                "file_upload" => [ "id" => $this->fileId ],
             ];
         }
 
@@ -104,18 +119,28 @@ class File
         return $this->type === FileType::Internal;
     }
 
+    public function isFileUpload(): bool
+    {
+        return $this->type === FileType::FileUpload;
+    }
+
     public function changeUrl(string $url): self
     {
-        return new self($this->type, $url, $this->expiryTime, $this->name, $this->caption);
+        return new self($this->type, $url, null, $this->expiryTime, $this->name, $this->caption);
+    }
+
+    public function changeFileUploadId(string $fileId): self
+    {
+        return new self($this->type, null, $fileId, $this->expiryTime, $this->name, $this->caption);
     }
 
     public function changeName(string $name): self
     {
-        return new self($this->type, $this->url, $this->expiryTime, $name, $this->caption);
+        return new self($this->type, $this->url, $this->fileId, $this->expiryTime, $name, $this->caption);
     }
 
     public function changeCaption(RichText ...$caption): self
     {
-        return new self($this->type, $this->url, $this->expiryTime, $this->name, $caption);
+        return new self($this->type, $this->url, $this->fileId, $this->expiryTime, $this->name, $caption);
     }
 }
